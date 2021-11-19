7b0215e142fd3e277b5733549bba5d1f33daa9af
HangaWilfried <wilfriedhanga5@gmail.com>
2021-11-19 14:42:33 +0100

    style: 07

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="sm:flex sm:flex-col sm:gap-0 sm:m-0 sm:content-center grid grid-cols-2 items-stretch gap-2 m-2">
+  <div class="sm:flex sm:flex-col sm:gap-0 sm:m-0 sm:content-center
+  md:grid md:grid-cols-2 md:items-stretch md:gap-2 md:m-2
+ lg:grid lg:grid-cols-2 lg:items-stretch lg:gap-2 lg:m-2
+ 2xl:grid 2xl:grid-cols-2 2xl:items-stretch 2xl:gap-2 2xl:m-2">
     <Header>
       <template #default>
-        <header class="col-start-1 row-start-1 border border-black bg-gray-900 text-white text-sm p-20 self-stretch">
+        <header class="md:col-start-1 md:row-start-1 md:self-stretch
+        lg:col-start-1 lg:row-start-1 lg:self-stretch
+        2xl:col-start-1 2xl:row-start-1 2xl:self-stretch
+        border border-black bg-gray-900 text-white text-sm p-20">
           <nav>
             <h1 class="text-white text-5xl font-bold italic">{{nextYear}} BIRTHDAYS APP</h1>
           </nav>
@@ -13,12 +19,19 @@
       </template>
     </Header>
 
-    <main class="sm:p-5 sm:flex sm:flex-col md:items-center bg-gray-100 md:px-64 md:py-10 md:col-start-2 md:col-end-3 md:row-start-1">
+    <main class="sm:p-5 sm:flex sm:flex-col bg-gray-100
+ md:items-center md:px-64 md:py-10 md:col-start-2 md:col-end-3 md:row-start-1
+ lg:items-center lg:px-64 lg:py-10 lg:col-start-2 lg:col-end-3 lg:row-start-1
+ 2xl:items-center 2xl:px-64 2xl:py-10 2xl:col-start-2 2xl:col-end-3 2xl:row-start-1">
       <Form @submit="addNewBirthday"/>
     </main>
   </div>
-  <div class="sm:flex sm:flex-col sm:content-center md:mx-2 md:mt-20">
-    <table class="border border-collapse bg-white md:w-full">
+  <div class="sm:flex sm:flex-col sm:content-center lg:mx-2 lg:mt-20
+xl:mx-2 lg:mt-20
+md:mx-2 lg:mt-20">
+    <table class="border border-collapse bg-white lg:w-full
+xl:w-full
+md:w-full">
       <tr>
         <th class="title">Name</th>
         <th class="title">Date of Birth</th>
@@ -49,7 +62,9 @@
             </td>
             <td class="cell text-purple-600">{{getRemaining(aboutBirthday(birthday.dateOfBirth))}}</td>
             <td class="cell text-gray-900">{{`${getTime().date} ${getTime().month} ${getTime().year}`}} At {{`${getHours().hour}:${getHours().minute}`}}</td>
-            <td class="cell"><div @click="deleteBirthday" class="md:ml-32 md:bg-red-500 text-white md:text-2xs rounded-full md:w-7 md:h-7 text-center">x</div></td>
+            <td class="cell"><div @click="deleteBirthday" class="text-white text-center rounded-full sm:ml-5 sm:bg-red-500 sm:w-6 sm:h-6 lg:text-2xs lg:ml-32 lg:bg-red-500 lg:w-7 lg:h-7
+md:text-2xs md:m-auto md:bg-red-500 md:w-7 md:h-7
+2xl:text-2xs 2xl:m-auto 2xl:bg-red-500 2xl:w-7 2xl:h-7">x</div></td>
           </tr>
         </template>
       </Birth>
